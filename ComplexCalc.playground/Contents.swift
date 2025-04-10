74c8c2929de316928ebbad84647ed6dfce01ca67
Amrith Gandham <amrithgandham@gmail.com>
2025-04-09 18:13:24 -0700

    Finish mathOp and handle tuples

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -41,8 +41,16 @@ class Calculator {
         return sum
     }
     
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
     
     func multiply(lhs: Int, rhs: Int) -> Int {
@@ -50,9 +58,9 @@ class Calculator {
     }
     
     func multiply(_ nums: [Int]) -> Int {
-        var product = 0
-        for num in nums {
-            product *= num
+        var product = 1
+        for i in 0..<nums.count {
+            product *= nums[i]
         }
         return product
     }
@@ -66,13 +74,26 @@ class Calculator {
     }
     
     func avg(_ nums: [Int]) -> Int {
-        var total = 0
-        for num in nums {
-            total *= num
+        var sum = 0
+        for i in 0..<nums.count {
+            sum += nums[i]
         }
-        return total / nums.count
+        return sum / nums.count
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var result = beg
+        for arg in args {
+            result = op(result, arg)
+        }
+        return result
     }
 }
+
 //: Don't change the name of this object (`calc`); it's used in all the tests.
 let calc = Calculator()
 
